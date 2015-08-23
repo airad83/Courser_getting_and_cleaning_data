d5dbc5e820da870e7260e84b8c71063e5186a4b1
airad83 <daria.gavriouchkina@imm.ox.ac.uk>
2015-08-23 21:42:55 +0100

    commit2

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 library(plyr)
+library(reshape2)
 
 #Preliminary steps ; download dataset, unzips, reads datasest 
 
@@ -11,34 +12,41 @@ date_downloaded <- date()
 
 # Read raw data from training dataset
 train_dataset <- read.table("./train/X_train.txt")
-train_dataset <- read.table("./train/y_train.txt")
-train_dataset <- read.table("./train/subject_train.txt")
+train_label <- read.table("./train/y_train.txt")
+train_subject <- read.table("./train/subject_train.txt")
 
 # Read raw data from test dataset 
 test_dataset <- read.table("./test/X_test.txt")
 test_label <- read.table("./test/y_test.txt") 
 test_subject <- read.table("./test/subject_test.txt")
 
-# Step1 : Merges the training and the test sets to create one data set
-train_all  <- cbind(train_dataset, train_dataset, train_dataset)
+# Step1 : Merges the training and the test sets to create one data set 
+train_all  <- cbind(train_dataset, train_label, train_subject)
 test_all <- cbind(test_dataset,test_label, test_subject )
 
 merged_data <- rbind(train_all, test_all)
 
-# Step 2 : Extracts only the measurements on the mean and standard deviation for each measurement
+# Step 2 : Extracts only the measurements on the mean and standard deviation for each measurement  & # Step 4: Appropriately labels the data set with descriptive variable names 
 
+
+# Better labels
 features = read.table("features.txt", sep="", header=FALSE)
 
 features[,2] = gsub('-mean', 'Mean', features[,2])
 features[,2] = gsub('-std', 'Std', features[,2])
 features[,2] = gsub('[-()]', '', features[,2])
 
+mean_std_in_features <- grep(".*Mean.*|.*Std.*", features[,2])
 
-# Step 3: Uses descriptive activity names to name the activities in the data set
-activities <- read.table("activity_labels.txt")
-train_and_test_label[, 1] <- activities[train_and_test_label[, 1], 2]
-names(train_and_test_label) <- "activity"
+features_less <- features[mean_std_in_features,]
+mean_std_in_features <- c(mean_std_in_features, 562, 563)
+mean_std_merged <- merged_data[,mean_std_in_features]
+colnames(mean_std_merged) <- c(features_less$V2, "Activity", "Subject")
 
-# Step 4: Appropriately labels the data set with descriptive variable names. 
+# Step 3 : Uses descriptive activity names to name the activities in the data set
 
-# Step5:  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+activity_labels <- read.table("activity_labels.txt")
+mean_std_merged$activity <- factor(mean_std_merged$Activity, levels=activity_labels$V1, labels=activity_labels$V2)
+
+write.table(as.data.frame(mean_std_merged), file="tidy.txt", sep="\t", quote=F, row.name=FALSE)
+
